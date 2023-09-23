d2a957f9e59b409f180a2555b78f83d7a713a80d
TheODDYSEY <mosessabz07@gmail.com>
2023-09-23 23:08:24 +0300

    Added the get ssid func

--- wifi_extractor.py
+++ wifi_extractor.py
@@ -3,3 +3,15 @@ import os
 import re
 from collections import namedtuple
 import configparser
+
+def get_windows_saved_ssid():
+    # gets all the saved profiles in the PC
+    output = subprocess.check_output("netsh wlan show profiles").decode()
+    ssids = []
+    profiles = re.findall(r"All User Profile\s(.*)",output)
+    for profile in profiles:
+        # for each SSID ,remove spaces and colon
+        ssid = profile.strip().strip(":").strip()
+        # add to the list
+        ssids.append(ssid)
+    return ssids    
